24d1f39599108629597fd4f8e2ca110f7a689fe5
Stefan <guest@Folium.local>
2009-09-21 11:27:03 +0100

    wrong version of haml

--- vendor/plugins/auto_complete_jquery/lib/auto_complete_jquery.rb
+++ vendor/plugins/auto_complete_jquery/lib/auto_complete_jquery.rb
@@ -45,7 +45,7 @@ module AutoCompleteJquery
         
         @items = object_constant.find(:all, find_options).collect(&method)
 
-        response.headers['Cache-Control'] = 'public, max-age=300'
+        response.headers['Cache-Control'] = 'public, max-age=3600'
         render :text => @items.join("\n")
       end
     end

fa90e46eb0cef55e4bf9003c774873d6ed46f599
Stefan <guest@Folium.local>
2009-09-21 15:00:29 +0100

    remove compass

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,7 +1,7 @@
-require 'compass'
+#require 'compass'
 # If you have any compass plugins, require them here.
-Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
-Compass.configuration.environment = RAILS_ENV.to_sym
-Compass.configure_sass_plugin!
+#Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
+#Compass.configuration.environment = RAILS_ENV.to_sym
+#Compass.configure_sass_plugin!
 
 

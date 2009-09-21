8a8c724e21251de4703e37a1c1b4885505e91632
Stefan <guest@Folium.local>
2009-09-21 11:12:07 +0100

    cache autocomplete

--- vendor/plugins/auto_complete_jquery/lib/auto_complete_jquery.rb
+++ vendor/plugins/auto_complete_jquery/lib/auto_complete_jquery.rb
@@ -45,6 +45,7 @@ module AutoCompleteJquery
         
         @items = object_constant.find(:all, find_options).collect(&method)
 
+        response.headers['Cache-Control'] = 'public, max-age=300'
         render :text => @items.join("\n")
       end
     end

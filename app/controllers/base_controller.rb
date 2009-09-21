a666f3310c055fc50e2df57b945f5f4dd40155c1
Stefan <guest@Folium.local>
2009-09-21 14:42:25 +0100

    rss news

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,3 +1,8 @@
+
+require 'rss/1.0'
+require 'rss/2.0'
+require 'open-uri'
+
 class BaseController < ApplicationController
   auto_complete_for :page, :title, :extra_conditions=> "pages.alias IS NOT true"
   auto_complete_for :ward, :name, :extra_conditions=> "wards.name IS NOT NULL"
@@ -15,6 +20,13 @@ class BaseController < ApplicationController
     rss = RSS::Parser.parse(content, false)
 
     @events_today = rss.items unless rss.blank?
+    
+    source = "http://birminghamnewsroom.com/?feed=rss2"
+    content = "" # raw content of rss feed will be loaded here
+    open(source) do |s| content = s.read end
+    rss = RSS::Parser.parse(content, false)
+
+    @news_today = rss.items unless rss.blank?
   end
   
 end

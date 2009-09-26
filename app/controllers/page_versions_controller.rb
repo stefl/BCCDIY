60402a9109e91427f562c9af4a089b6d9d9533e5
Stefan <guest@Folium.local>
2009-09-26 18:02:19 +0100

    home layout

--- app/controllers/page_versions_controller.rb
+++ app/controllers/page_versions_controller.rb
@@ -1,3 +1,10 @@
-class PagesController < ResourceController::Base
-
+class PageVersionsController < ResourceController::Base
+  index.wants.rss {
+      render_rss_feed_for(@page_versions, { :feed => {:title => "BCCDIY Recent edits", :link => page_versions_url},
+                            :item => {:title => Proc.new {|page| page.title + " updated by " + page.user.login},
+                                      :link =>  Proc.new {|page| page_path(page)},
+                                       :description => Proc.new {|page| @page.is_textile ? textilize(page.content) : page.content},                                       
+                                       :pub_date => :created_at}
+      })
+  }
 end

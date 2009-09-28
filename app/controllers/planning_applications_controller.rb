30b758161e313c3dbbb5a7c24bcab257ebe5ea9e
Stefan <guest@internal.RFC1918.telecomplete.net>
2009-09-28 13:56:18 +0100

    planning applications

--- app/controllers/planning_applications_controller.rb
+++ app/controllers/planning_applications_controller.rb
@@ -55,4 +55,10 @@ class PlanningApplicationsController < ResourceController::Base
   }
   
   
+  private
+     def collection
+       params[:page] = 1 if params[:page].blank?
+       @collection ||= end_of_association_chain.paginate :page => params[:page], :order => 'id DESC'
+       
+     end
 end

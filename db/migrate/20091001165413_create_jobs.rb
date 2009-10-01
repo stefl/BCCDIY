57ebd3770d720affe3879bd0f6930bd74330acf4
Stef Lewandowski <stef@stef.io>
2009-10-01 21:40:23 +0100

    nicer near me page css

--- db/migrate/20091001165413_create_jobs.rb
+++ db/migrate/20091001165413_create_jobs.rb
@@ -1,12 +1,15 @@
 class CreateJobs < ActiveRecord::Migration
   def self.up
     create_table :jobs do |t|
-
+      
       t.timestamps
     end
+    
+    add_column :users, :postcode, :string
   end
 
   def self.down
     drop_table :jobs
+    remove_column :users, :postcode
   end
 end

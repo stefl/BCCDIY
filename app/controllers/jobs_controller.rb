9aa52bab910342b1e469c23ebd59b80213e80f75
Stef Lewandowski <stef@stef.io>
2009-10-02 12:31:10 +0100

    jobs as rss

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,8 +1,35 @@
 class JobsController < ResourceController::Base
+  
+  index.wants.rss {
+      render_rss_feed_for(@jobs, { :feed => {:title => "Jobs with Birmingham City Council", :link => jobs_url},
+                            :item => {:title => :title,
+                                      :link =>  Proc.new {|job| job_url(job)},
+                                       :description => :description
+                                         },                                       
+                                       :pub_date => :updated_at
+      })
+  }
+  
+  index.wants.xml{
+   render :xml=>@jobs
+  }
+  
+  index.wants.json{
+   render :json=>@jobs
+  }
+  
+  show.wants.xml{
+   render :xml=>@job
+  }
+  
+  show.wants.json{
+   render :json=>@job
+  }
+  
   private
      def collection
        params[:page] = 1 if params[:page].blank?
-       @collection ||= end_of_association_chain.paginate :page => params[:page], :order => 'closing_date DESC'
+       @collection ||= end_of_association_chain.paginate :page => params[:page], :order => 'closing_date DESC', :conditions=>'title is not null'
        
      end
 end

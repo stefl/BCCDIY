b4d40d538ccb45a9fc62526556d0874ec2eb69ce
Stef Lewandowski <stef@stef.io>
2009-10-05 21:36:29 +0100

    xml and json for a few more pages

--- app/controllers/restyles_controller.rb
+++ app/controllers/restyles_controller.rb
@@ -9,4 +9,9 @@ class RestylesController < ResourceController::Base
     cookies[:restyle_id] = nil
     redirect_to restyles_path
   end
+  
+  show.wants.xml {render :xml=>@restyle}
+  show.wants.json {render :json=>@restyle}
+  index.wants.xml {render :xml=>@restyles}
+  index.wants.json {render :json=>@restyles}
 end
--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,2 +1,6 @@
 class ToolsController < ResourceController::Base
+  show.wants.xml {render :xml=>@tool}
+  show.wants.json {render :json=>@tool}
+  index.wants.xml {render :xml=>@tools}
+  index.wants.json {render :json=>@tools}
 end
